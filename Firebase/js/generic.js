fe3e0afad457ab4834a2b268c67fea463e8232da
Jéssica Bustamante <jessica-bustamante@outlook.com>
2021-10-20 20:48:46 -0500

    Add files via upload

---
+++ Firebase/js/generic.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAHteEyog2X9UawjzroBLWZhejZON8_luc",
+    authDomain: "fir-proyecto1-d752f.firebaseapp.com",
+    projectId: "fir-proyecto1-d752f",
+    storageBucket: "fir-proyecto1-d752f.appspot.com",
+    messagingSenderId: "982961179904",
+    appId: "1:982961179904:web:fa67e07c7022a9a64577e5",
+    measurementId: "G-74HBD63MNJ"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
